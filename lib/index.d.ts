88dd7eec83e2bc4ad6066e523ac7d8ada98c6d74
yosef langer <yosef@doar.systems>
2025-05-17 07:35:32 +0900

    fixes

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,6 +3,16 @@ export interface Proxy {
     port: number;
     protocol: string;
     country: string;
+    latency: number;
 }
-export declare function getProxies(): Promise<Proxy[]>;
+export declare enum ProxyProtocol {
+    'HTTP' = "HTTP",
+    'HTTPS' = "HTTPS",
+    'SOCKS' = "SOCKS"
+}
+interface GetProxiesOptions {
+    protocol?: ProxyProtocol;
+}
+export declare function getProxies(otps?: GetProxiesOptions): Promise<Proxy[]>;
+export {};
 //# sourceMappingURL=index.d.ts.map
